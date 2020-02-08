e9acdb305b074638161e8c10c8061132b111dd13
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-02-08 12:13:58 +0500

    changes kr k check kr rha tha RidesFragment mae.

--- app/src/main/java/com/sohaibaijaz/sawaari/MainActivity.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyCxh6jiboDAWzR7c_373KDStrtj2W4Sgg4";
 
-    public static String baseurl= "https://maaz-ypf5.localhost.run";
+    public static String baseurl= "http://52.15.104.184:8000";
 
     private int backpress = 0;
     @Override
