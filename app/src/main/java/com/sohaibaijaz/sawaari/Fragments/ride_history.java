6b8bc2d6da044c9b6ba39a3c413f6c2d1b7f05e7
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-02-20 13:46:22 +0500

    App working fine and Ride not available activity update.

---
+++ app/src/main/java/com/sohaibaijaz/sawaari/Fragments/ride_history.java
@@ -0,0 +1,22 @@
+package com.sohaibaijaz.sawaari.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.sohaibaijaz.sawaari.R;
+
+public class ride_history extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.history_ride, container, false);
+    }
+
+}
