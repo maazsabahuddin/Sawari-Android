5223e9eca617cf211a9b10d373e8d41db8fd0a0f
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-03-01 12:53:38 +0500

    boarding pass details update.

--- app/src/main/java/com/sohaibaijaz/sawaari/UserRidesDetailActivity.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/UserRidesDetailActivity.java
@@ -4,15 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.sohaibaijaz.sawaari.Fragments.RideFragmentN;
-
-import java.util.Objects;
 
 public class UserRidesDetailActivity extends AppCompatActivity {
 
@@ -24,6 +19,10 @@ public class UserRidesDetailActivity extends AppCompatActivity {
     private TextView tv_ride_seat_icon;
     private TextView green_icon;
     private TextView red_icon;
+    private TextView return_trip_icon;
+    private TextView invoice_icon;
+    private TextView share_btn_icon;
+    private TextView cancel_btn_icon;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -34,11 +33,15 @@ public class UserRidesDetailActivity extends AppCompatActivity {
 
         b = getIntent().getExtras();
         back_btn_ride_details = findViewById(R.id.back_btn_ride_details);
-        my_ride_details_tv = findViewById(R.id.my_ride_details_tv);
+        my_ride_details_tv = findViewById(R.id.boarding_pass_details_tv);
         tv_ride_seat_icon = findViewById(R.id.tv_ride_seat_icon);
         tv_ride_seats = findViewById(R.id.tv_ride_seats);
         green_icon = findViewById(R.id.green_icon);
         red_icon = findViewById(R.id.red_icon);
+        return_trip_icon = findViewById(R.id.return_trip_icon);
+        invoice_icon = findViewById(R.id.invoice_icon);
+        share_btn_icon = findViewById(R.id.share_btn_icon);
+        cancel_btn_icon = findViewById(R.id.cancel_btn_icon);
 
         final String ride_status = b.getString("ride_status");
         final String ride_fare = b.getString("ride_fare");
@@ -55,6 +58,10 @@ public class UserRidesDetailActivity extends AppCompatActivity {
         tv_ride_seat_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.seatinblue, 0, 0, 0);
         green_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.greencircle, 0, 0, 0);
         red_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.red_circle, 0, 0, 0);
+        return_trip_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.shareicon, 0, 0, 0);
+        invoice_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.invoiceicon, 0, 0, 0);
+        share_btn_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.shareicon, 0, 0, 0);
+        cancel_btn_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.cancelbtnicon, 0, 0, 0);
         tv_ride_seats.setText(ride_seats);
 
         back_btn_ride_details.setOnClickListener(new View.OnClickListener() {
