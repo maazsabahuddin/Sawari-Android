70a257867887774375afe6b1f5ded7f7c547e94c
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-03-09 16:39:56 +0500

    final booking activity ui done.

--- app/src/main/java/com/sohaibaijaz/sawaari/Rides/SelectedRideActivity.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/Rides/SelectedRideActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
@@ -17,12 +18,29 @@ import android.widget.Toast;
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.sohaibaijaz.sawaari.BookingActivity;
 import com.sohaibaijaz.sawaari.MainActivity;
 import com.sohaibaijaz.sawaari.R;
+import com.sohaibaijaz.sawaari.UserDetails;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelectedRideActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
+    private RequestQueue requestQueue;
     private Bundle b;
     private int FARE_PER_SEAT = 50;
     private String FARE_PER_PERSON = (FARE_PER_SEAT + " x 1");
@@ -35,6 +53,8 @@ public class SelectedRideActivity extends AppCompatActivity {
         setContentView(R.layout.select_ride);
         getSupportActionBar().hide();
 
+        requestQueue = Volley.newRequestQueue(this);
+
         final TextView day_and_date = findViewById(R.id.day_and_date);
         final TextView ride_route_name = findViewById(R.id.ride_route_name);
         final TextView ride_seats_left = findViewById(R.id.ride_seats_left);
@@ -46,9 +66,10 @@ public class SelectedRideActivity extends AppCompatActivity {
         final TextView drop_off_time = findViewById(R.id.drop_off_time);
         final TextView drop_off_location_tv = findViewById(R.id.drop_off_location_tv);
         final TextView drop_off_walking_mints = findViewById(R.id.drop_off_walking_mints);
-        final TextView fare_per_person = findViewById(R.id.fare_per_person);
+//        final TextView fare_per_person = findViewById(R.id.fare_per_person);
+        final TextView seats_icon = findViewById(R.id.seats_icon);
 
-        final TextView total_fare = findViewById(R.id.total_fare);
+//        final TextView total_fare = findViewById(R.id.total_fare);
         final TextView subtract_seat = findViewById(R.id.subtract_seat);
         final TextView total_seats = findViewById(R.id.total_seats);
         final TextView add_seats = findViewById(R.id.add_seats);
@@ -98,7 +119,7 @@ public class SelectedRideActivity extends AppCompatActivity {
         walking_icon_drop_off.setCompoundDrawablesWithIntrinsicBounds(R.drawable.walk, 0, 0, 0);
         pick_up_circle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.pickupinblue, 0, 0, 0);
         drop_off_circle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.dropoffinblue, 0, 0, 0);
-//        dot_line.setCompoundDrawablesWithIntrinsicBounds(R.drawable.vertical_line_96, 0, 0, 0);
+        seats_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.blue_seat_icon_40px, 0, 0, 0);
         price_method_icon.setCompoundDrawablesWithIntrinsicBounds(R.drawable.money, 0, 0, 0);
 
         final Spinner spinner = findViewById(R.id.payment_spinner);
@@ -134,37 +155,37 @@ public class SelectedRideActivity extends AppCompatActivity {
         });
 
         subtract_seat.setEnabled(false);
-        total_fare.setText(String.valueOf(FARE_PER_SEAT));
-        fare_per_person.setText("(" + FARE_PER_PERSON + ")");
+//        total_fare.setText(String.valueOf(FARE_PER_SEAT));
+//        fare_per_person.setText("(" + FARE_PER_PERSON + ")");
         subtract_seat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int seat_value = Integer.parseInt(total_seats.getText().toString());
                 String fare_per_person_value;
                 if(seat_value==1){
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     subtract_seat.setEnabled(false);
                     add_seats.setEnabled(true);
                 }
                 else if(seat_value==2){
                     seat_value -= 1;
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     total_seats.setText(String.valueOf(seat_value));
                     subtract_seat.setEnabled(false);
                     add_seats.setEnabled(true);
                 }
                 else if(seat_value==3){
                     seat_value -= 1;
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     total_seats.setText(String.valueOf(seat_value));
                     add_seats.setEnabled(true);
                 }
@@ -177,28 +198,28 @@ public class SelectedRideActivity extends AppCompatActivity {
                 int seat_value = Integer.parseInt(total_seats.getText().toString());
                 String fare_per_person_value;
                 if(seat_value==3){
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     subtract_seat.setEnabled(true);
                     add_seats.setEnabled(false);
                 }
                 else if(seat_value==1){
                     seat_value += 1;
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     total_seats.setText(String.valueOf(seat_value));
                     subtract_seat.setEnabled(true);
                 }
                 else if(seat_value==2){
                     seat_value += 1;
-                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
-                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
-                    fare_per_person.setText("(" + fare_per_person_value + ")");
-                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
+//                    fare_per_person_value = FARE_PER_SEAT + " x " + seat_value;
+//                    fare_per_person. setTextColor(Color.parseColor("#1F3A93"));
+//                    fare_per_person.setText("(" + fare_per_person_value + ")");
+//                    total_fare.setText(String.valueOf(FARE_PER_SEAT*seat_value));
                     total_seats.setText(String.valueOf(seat_value));
                     subtract_seat.setEnabled(true);
                     add_seats.setEnabled(false);
@@ -209,7 +230,91 @@ public class SelectedRideActivity extends AppCompatActivity {
         book_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Working Activity", Toast.LENGTH_SHORT).show();
+
+                final String no_of_seats = total_seats.getText().toString();
+                final String payment_type = spinner.getSelectedItem().toString();
+
+                try {
+                    String URL = MainActivity.baseurl+"/book_ride/";
+//                    spinner.setVisibility(View.VISIBLE);
+//                    spinner_frame.setVisibility(View.VISIBLE);
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+//                            spinner.setVisibility(View.GONE);
+//                            spinner_frame.setVisibility(View.GONE);
+
+                            Log.i("VOLLEY", response.toString());
+                            try {
+                                final JSONObject json = new JSONObject(response);
+                                if (json.getString("status").equals("200")) {
+                                    Toast.makeText(getApplicationContext(), json.getString("status"), Toast.LENGTH_LONG).show();
+                                }
+                                else if (json.getString("status").equals("400")||json.getString("status").equals("404")) {
+                                    Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                Log.e("VOLLEY", e.toString());
+                                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+//                            spinner.setVisibility(View.GONE);
+//                            spinner_frame.setVisibility(View.GONE);
+                            Toast.makeText(getApplicationContext(), "Server is temporarily down, sorry for your inconvenience", Toast.LENGTH_SHORT).show();
+                            Log.e("VOLLEY", error.toString());
+                        }
+                    }){
+                        @Override
+                        protected Map<String,String> getParams(){
+                            Map<String,String> params = new HashMap<String, String>();
+                            params.put("vehicle_no_plate",vehicle_no_plate);
+                            params.put("req_seats",no_of_seats);
+                            params.put("pick_up_point_stop_id", pickup_location_id);
+                            params.put("drop_up_point_stop_id", dropoff_location_id);
+                            params.put("payment_method", payment_type);
+                            params.put("ride_date", ride_date);
+                            params.put("route_id", route_name);
+                            params.put("arrival_time", arrival_time);
+                            params.put("departure_time", departure_time);
+//                                params.put(KEY_EMAIL, email);
+                            return params;
+                        }
+
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            Map<String, String> headers = new HashMap<String, String>();
+                            headers.put("Authorization", token);
+                            return headers;
+                        }
+                    };
+
+                    stringRequest.setRetryPolicy(new RetryPolicy() {
+                        @Override
+                        public int getCurrentTimeout() {
+                            return 50000;
+                        }
+
+                        @Override
+                        public int getCurrentRetryCount() {
+                            return 50000;
+                        }
+
+                        @Override
+                        public void retry(VolleyError error) throws VolleyError {
+
+                        }
+                    });
+                    requestQueue.add(stringRequest);
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
             }
         });
 
