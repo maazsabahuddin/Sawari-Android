75fbf03320c9259dec9a4efc5db65569285f4c82
shahrukhmahmood <59915344+shahrukhmahmood@users.noreply.github.com>
2020-04-20 02:57:04 +0500

    safety

---
+++ app/src/main/java/com/sohaibaijaz/sawaari/Settings/UpdateHomeAndWorkActivity.java
@@ -0,0 +1,16 @@
+package com.sohaibaijaz.sawaari.Settings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sohaibaijaz.sawaari.R;
+
+public class UpdateHomeAndWorkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_home_and_work);
+    }
+}
