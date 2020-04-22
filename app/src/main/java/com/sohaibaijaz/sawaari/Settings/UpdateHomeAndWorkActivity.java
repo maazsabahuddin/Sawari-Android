876ced0ae9b4e062eb0e425f36e5b631d522cd2a
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-04-22 22:36:32 +0500

    fragment functionality done for UpdateHome and Work

--- app/src/main/java/com/sohaibaijaz/sawaari/Settings/UpdateHomeAndWorkActivity.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/Settings/UpdateHomeAndWorkActivity.java
@@ -1,9 +1,13 @@
 package com.sohaibaijaz.sawaari.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.sohaibaijaz.sawaari.Maps.LocationFragment;
 import com.sohaibaijaz.sawaari.R;
 
 public class UpdateHomeAndWorkActivity extends AppCompatActivity {
@@ -11,8 +15,22 @@ public class UpdateHomeAndWorkActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_home_and_work);
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_update_home_work);
+        getSupportActionBar().hide();
+//        getSupportActionBar().setTitle("");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (savedInstanceState == null) {
+            Fragment fragment = new UpdateHomeWorkFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.HomeWorkFragment, fragment).commit();
+        }
+
+        TextView BackLA = findViewById(R.id.BackHomeWorkButton);
+        BackLA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
