363fd720469f5095e122bd8db9eb2c79efed4aaa
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-04-29 03:21:40 +0500

    just create saved places fragment

---
+++ app/src/main/java/com/sohaibaijaz/sawaari/Fragments/SavePlacesFragment.java
@@ -0,0 +1,16 @@
+package com.sohaibaijaz.sawaari.Fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sohaibaijaz.sawaari.R;
+
+public class SavePlacesFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_places_fragment);
+    }
+}
