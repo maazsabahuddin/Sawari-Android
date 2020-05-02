ed0fcb9fc22418876b59ca9a260ad3f3424f3b75
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-05-03 03:29:08 +0500

    login_Verify activity create.

---
+++ app/src/main/java/com/sohaibaijaz/sawaari/Login/LoginVerify.java
@@ -0,0 +1,16 @@
+package com.sohaibaijaz.sawaari.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sohaibaijaz.sawaari.R;
+
+public class LoginVerify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_verify);
+    }
+}
