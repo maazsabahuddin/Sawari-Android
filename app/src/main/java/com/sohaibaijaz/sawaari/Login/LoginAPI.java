dc2b79c5e59266060bf7085236c7c6a4d3dccca9
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-05-03 07:35:57 +0500

    password update activity update.

--- app/src/main/java/com/sohaibaijaz/sawaari/Login/LoginAPI.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/Login/LoginAPI.java
@@ -1,13 +1,16 @@
 package com.sohaibaijaz.sawaari.Login;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -16,6 +19,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.sohaibaijaz.sawaari.Fragments.CallBack;
+import com.sohaibaijaz.sawaari.MainActivity;
 import com.sohaibaijaz.sawaari.Settings.SettingsFragment;
 
 import org.json.JSONException;
@@ -23,6 +27,7 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import static com.sohaibaijaz.sawaari.Login.LoginPin.app;
 import static com.sohaibaijaz.sawaari.Login.LoginPin.baseurl;
@@ -96,5 +101,81 @@ public class LoginAPI {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void updatePassword(final Activity context, final String pin, final CallBack callBack){
+        final SharedPreferences sharedPreferences= Objects.requireNonNull(context).getSharedPreferences(MainActivity.AppPreferences, Context.MODE_PRIVATE);
+        final RequestQueue requestQueue = Volley.newRequestQueue(context);
+        final String token = sharedPreferences.getString("Token", "");
+
+        if(!token.equals("")) {
+            try {
+                String URL = baseurl+"/password/change/";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.i("VOLLEY", response);
+                        try {
+                            JSONObject json = new JSONObject(response);
+                            if (json.getString("status").equals("200")) {
+                                callBack.onSuccess(json.getString("status"), json.getString("message"));
+                            }
+                            else{
+                                callBack.onFailure(json.getString("status"), json.getString("message"));
+                            }
+
+                        } catch (JSONException e) {
+                            Log.e("VOLLEY", e.toString());
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("VOLLEY", error.toString());
+                    }
+                }){
+                    @Override
+                    protected Map<String,String> getParams(){
+                        Map<String,String> params = new HashMap<>();
+                        params.put("pin", pin);
+                        return params;
+                    }
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String, String> headers = new HashMap<String, String>();
+                        headers.put("Authorization", token);
+                        return headers;
+                    }
+
+                };
+
+                stringRequest.setRetryPolicy(new RetryPolicy() {
+                    @Override
+                    public int getCurrentTimeout() {
+                        return 5000;
+                    }
+
+                    @Override
+                    public int getCurrentRetryCount() {
+                        return 5000;
+                    }
+
+                    @Override
+                    public void retry(VolleyError error) throws VolleyError {
+
+                    }
+                });
+                requestQueue.add(stringRequest);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            Toast.makeText(context, "There was problem connecting to the server", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/sohaibaijaz/sawaari/Settings/Updatepassword.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/Settings/Updatepassword.java
@@ -1,7 +1,10 @@
 package com.sohaibaijaz.sawaari.Settings;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +25,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.sohaibaijaz.sawaari.Fragments.CallBack;
+import com.sohaibaijaz.sawaari.Login.LoginAPI;
 import com.sohaibaijaz.sawaari.MainActivity;
+import com.sohaibaijaz.sawaari.NavActivity;
 import com.sohaibaijaz.sawaari.R;
 
 import org.json.JSONException;
@@ -34,41 +41,62 @@ import static com.sohaibaijaz.sawaari.MainActivity.AppPreferences;
 
 public class Updatepassword extends AppCompatActivity {
 
-    Button button_update_password;
+    ImageView button_update_password;
     EditText editText_new_password;
-    EditText editText_confirm_password;
     SharedPreferences sharedPreferences;
-    TextView txt_error;
+    TextView error_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("Password");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().hide();
+//        getSupportActionBar().setTitle("Password");
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_updatepassword);
 
         sharedPreferences = Updatepassword.this.getSharedPreferences(AppPreferences, Context.MODE_PRIVATE);
 
-        button_update_password=findViewById(R.id.update_password);
-        editText_new_password=findViewById(R.id.new_password);
-        editText_confirm_password=findViewById(R.id.confirm_password);
-        txt_error=findViewById(R.id.errormessage);
+        button_update_password = findViewById(R.id.update_password);
+        editText_new_password = findViewById(R.id.pin);
+        error_message = findViewById(R.id.error_message_pin);
 
-        button_update_password.setOnClickListener(new View.OnClickListener() {
+        editText_new_password.requestFocus();
+        error_message.setVisibility(View.GONE);
+        editText_new_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //checkkrlo.setVisibility(View.VISIBLE);
-                editText_confirm_password.setCursorVisible(false);
-                editText_new_password.setCursorVisible(false);
-                update_password();
+                editText_new_password.setCursorVisible(true);
+                error_message.setVisibility(View.GONE);
             }
         });
 
-        editText_confirm_password.setOnClickListener(new View.OnClickListener() {
+        button_update_password.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
-                editText_confirm_password.setCursorVisible(true);
-                txt_error.setVisibility(View.GONE);
+                editText_new_password.setCursorVisible(false);
+                final String password_new = editText_new_password.getText().toString();
+
+                if(password_new.equals("")){
+                    error_message.setVisibility(View.VISIBLE);
+                    error_message.setText("Password fields cannot be empty");
+                }
+                else{
+                    LoginAPI.updatePassword(Updatepassword.this, password_new, new CallBack() {
+                        @Override
+                        public void onSuccess(String status_code, String message) {
+                            Toast.makeText(Updatepassword.this, message, Toast.LENGTH_LONG).show();
+                            Intent i = new Intent(Updatepassword.this, NavActivity.class);
+                            Updatepassword.this.startActivity(i);
+                        }
+
+                        @Override
+                        public void onFailure(String status_code, String message) {
+                            error_message.setVisibility(View.VISIBLE);
+                            error_message.setText(message);
+                        }
+                    });
+                }
             }
         });
 
@@ -76,11 +104,11 @@ public class Updatepassword extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 editText_new_password.setCursorVisible(true);
-                txt_error.setVisibility(View.GONE);
+                error_message.setVisibility(View.GONE);
             }
         });
-
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
@@ -91,207 +119,4 @@ public class Updatepassword extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    public void update_password(){
-
-        final String password_new = editText_new_password.getText().toString();
-        final String password_confirm = editText_confirm_password.getText().toString();
-
-        if(password_confirm.equals("") || password_new.equals("")){
-
-            txt_error.setVisibility(View.VISIBLE);
-            txt_error.setText("Password fields cannot be empty");
-            editText_new_password.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    txt_error.setVisibility(View.GONE);
-                    editText_new_password.setCursorVisible(true);
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-
-            editText_confirm_password.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    txt_error.setVisibility(View.GONE);
-                    editText_confirm_password.setCursorVisible(true);
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-        }
-        else if(!password_new.equals(password_confirm)) {
-
-            txt_error.setVisibility(View.VISIBLE);
-            txt_error.setText("Password fields are not same");
-
-            editText_new_password.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    txt_error.setVisibility(View.GONE);
-                    editText_new_password.setCursorVisible(true);
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-
-            editText_confirm_password.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    txt_error.setVisibility(View.GONE);
-                    editText_confirm_password.setCursorVisible(true);
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-        }
-
-        else {
-
-            try {
-
-                String url = MainActivity.baseurl+"/password/change/";
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            //@RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                            @Override
-                            public void onResponse(String response) {
-
-
-                                try {
-                                    JSONObject json = new JSONObject(response);
-                                    if (json.getString("status").equals("200")) {
-
-
-                                        // SharedPreferences.Editor editor = sharedPreferences.edit();
-                                        //editor.putString("Token", token);
-                                        //editor.apply();
-
-                                        Toast.makeText(Updatepassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
-
-
-                                    } else if (json.getString("status").equals("401") || json.getString("status").equals("405") || json.getString("status").equals("406") || json.getString("status").equals("407")) {
-
-                                        Toast.makeText(Updatepassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
-                                    }
-                                    else if(json.getString("status").equals("404")){
-                                        Toast.makeText(Updatepassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
-                                        SettingsFragment.forcedLogout(Updatepassword.this);
-                                        // flag = false;
-                                    }
-                                    else{
-
-                                        txt_error.setVisibility(View.VISIBLE);
-                                        txt_error.setText(json.getString("message"));
-                                        //Toast.makeText(Updatepassword.this, json.getString("message"), Toast.LENGTH_LONG).show();
-                                        editText_new_password.addTextChangedListener(new TextWatcher() {
-                                            @Override
-                                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                                            }
-
-                                            @Override
-                                            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                                                txt_error.setVisibility(View.GONE);
-                                            }
-
-                                            @Override
-                                            public void afterTextChanged(Editable s) {
-
-                                            }
-                                        });
-
-                                        editText_confirm_password.addTextChangedListener(new TextWatcher() {
-                                            @Override
-                                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                                            }
-
-                                            @Override
-                                            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                                                txt_error.setVisibility(View.GONE);
-                                            }
-
-                                            @Override
-                                            public void afterTextChanged(Editable s) {
-
-                                            }
-                                        });
-                                    }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(Updatepassword.this, error.toString(), Toast.LENGTH_LONG).show();
-                            }
-                        }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        Map<String, String> params = new HashMap<>();
-                        params.put("pin", password_new);
-                        params.put("confirm_pin", password_confirm);
-
-                        return params;
-                    }
-
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        Map<String, String> headers = new HashMap<String, String>();
-                        headers.put("Authorization", sharedPreferences.getString("Token", ""));
-                        return headers;
-                    }
-
-                };
-
-                RequestQueue requestQueue = Volley.newRequestQueue(this);
-                requestQueue.add(stringRequest);
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-    }
 }
