ce79070d0314bc2bfe29f102041ebf8eedeb4174
Maaz Sabahuddin <maazsabahuddin@gmail.com>
2020-05-11 19:06:14 +0500

    forgot password xml update..

--- app/src/main/java/com/sohaibaijaz/sawaari/ForgetPasswordActivity.java
+++ app/src/main/java/com/sohaibaijaz/sawaari/ForgetPasswordActivity.java
@@ -11,6 +11,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -33,76 +34,69 @@ import java.util.Map;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
 
-    private EditText txt_email_phone;
-    private Button btn_send;
+    private EditText email_or_phone;
+    private ImageView sendEmailButton;
+    private TextView resend_email;
+    private TextView error_message_forgot_password;
 
-    private FrameLayout spinner_frame;
-    private ProgressBar spinner;
+//    private FrameLayout spinner_frame;
+//    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
         getSupportActionBar().hide();
-        txt_email_phone = findViewById(R.id.txt_email_phone);
-        btn_send = findViewById(R.id.btn_send);
-        spinner = (ProgressBar) findViewById(R.id.progressBar1);
-        spinner.setVisibility(View.GONE);
-        spinner_frame = findViewById(R.id.spinner_frame);
-        spinner_frame.setVisibility(View.GONE);
+
+        error_message_forgot_password = findViewById(R.id.error_message_forgot_password);
+        error_message_forgot_password.setVisibility(View.GONE);
+
+        resend_email = findViewById(R.id.resend_email);
+        resend_email.setAlpha(0.5f);
+        resend_email.setEnabled(false);
+
+        email_or_phone = findViewById(R.id.email_or_phone);
+        sendEmailButton = findViewById(R.id.sendEmailButton);
+
         final RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
 
-        txt_email_phone.setOnEditorActionListener(new EditText.OnEditorActionListener(){
-
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if (i == EditorInfo.IME_ACTION_DONE) {
-                    btn_send.performClick();
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(btn_send.getWindowToken(),
-                            InputMethodManager.RESULT_UNCHANGED_SHOWN);
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        btn_send.setOnClickListener(new View.OnClickListener() {
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                final String email_phone = txt_email_phone.getText().toString();
+                final String email_phone = email_or_phone.getText().toString();
 
                 if (email_phone.equals("")) {
                     Toast.makeText(getApplicationContext(), "Please enter email or phone number", Toast.LENGTH_SHORT).show();
-                } else if (!email_phone.equals("")) {
+                }
+                else{
                     try {
-                        String URL = MainActivity.baseurl + "/password/reset/";
+                        String URL = MainActivity.baseurl + "/forgot/password/";
                         JSONObject jsonBody = new JSONObject();
-                        jsonBody.put("email_or_phone", email_phone);
-                        final String requestBody = jsonBody.toString();
-                        spinner.setVisibility(View.VISIBLE);
-                        spinner_frame.setVisibility(View.VISIBLE);
+//                        jsonBody.put("email_or_phone", email_phone);
+//                        final String requestBody = jsonBody.toString();
+
                         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                spinner.setVisibility(View.GONE);
-                                spinner_frame.setVisibility(View.GONE);
-                                Log.i("VOLLEY", response.toString());
+
+                                Log.i("VOLLEY", response);
                                 try {
                                     JSONObject json = new JSONObject(response);
                                     if (json.getString("status").equals("200")) {
 
-                                        String token_uuid = json.getString("token_uuid");
-                                        Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
-                                        Intent myIntent = new Intent(getApplicationContext(), ForgetPasswordVerify.class);
-                                        myIntent.putExtra("token_uuid", token_uuid);
-                                        myIntent.putExtra("email_or_phone", email_phone);
+                                        error_message_forgot_password.setText(json.getString("message"));
+//                                        String token_uuid = json.getString("token_uuid");
+//                                        Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
+//                                        Intent myIntent = new Intent(getApplicationContext(), ForgetPasswordVerify.class);
+//                                        myIntent.putExtra("token_uuid", token_uuid);
+//                                        myIntent.putExtra("email_or_phone", email_phone);
+//
+//                                        startActivity(myIntent);
 
-                                        startActivity(myIntent);
-
-                                    } else if (json.getString("status").equals("400") || json.getString("status").equals("404")) {
-                                        Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
+                                    } else if (json.getString("status").equals("400")) {
+                                        error_message_forgot_password.setText(json.getString("message"));
+//                                        Toast.makeText(getApplicationContext(), json.getString("message"), Toast.LENGTH_SHORT).show();
                                     }
                                 } catch (JSONException e) {
                                     Log.e("VOLLEY", e.toString());
@@ -142,14 +136,9 @@ public class ForgetPasswordActivity extends AppCompatActivity {
 
                         requestQueue.add(stringRequest);
 
-                    } catch (JSONException e) {
-                        spinner.setVisibility(View.GONE);
-                        spinner_frame.setVisibility(View.GONE);
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
-                } else {
-
-                    Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_SHORT).show();
                 }
 
             }
